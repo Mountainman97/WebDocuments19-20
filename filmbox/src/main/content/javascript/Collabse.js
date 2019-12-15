163cc26f057f6fbf57db940061619e287598e8cc
Badr <Badr.aldein.alzein@gmail.com>
2019-12-15 20:10:29 +0100

    moving to html

--- filmbox/src/main/content/javascript/Collabse.js
+++ filmbox/src/main/content/javascript/Collabse.js
@@ -1,25 +1,24 @@
 /*used in FAQ
  */
 function showOnClick(id) {
-	var x = document.getElementById(id);
-	if (x.style.display === "block") {
-		x.style.display = "none";
-	} else {
-		x.style.display = "block";
-	}
+    var x = document.getElementById(id);
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
 }
 
-
 function showlist() {
-	var list = document.getElementById("movielist");
-	var orangePanner = document.getElementById("OrangePanner");
-	list.style.display = "block";
-	orangePanner.style.display = "none"
+    var list = document.getElementById("movielist");
+    var orangePanner = document.getElementById("OrangePanner");
+    list.style.display = "block";
+    orangePanner.style.display = "none"
 }
 
 function hidelist() {
-	var list = document.getElementById("movielist");
-	var orangePanner = document.getElementById("OrangePanner");
-	list.style.display = "none";
-	orangePanner.style.display = "block"
+    var list = document.getElementById("movielist");
+    var orangePanner = document.getElementById("OrangePanner");
+    list.style.display = "none";
+    orangePanner.style.display = "block"
 }
