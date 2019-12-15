7673f82a711edb2691e2764c06f62c525d454439
Max Bergmann <bergmmax@b-tu.de>
2019-12-15 18:36:45 +0100

    added xml to Ranked Page (v1)

--- filmbox/src/main/content/javascript/Information.js
+++ filmbox/src/main/content/javascript/Information.js
@@ -2,14 +2,14 @@ window.addEventListener("load", function (evt) {
     
     var request = new XMLHttpRequest();
 
-	request.onreadystatechange = function() {
+    request.open("GET", "https://mountainman97.github.io/WebDocuments19-20/filmbox/src/main/resources/test.xml", true);
+    request.send();
+
+    request.onreadystatechange = function() {
 		if (request.readyState == 4 && request.status == 200) {
 			fillInformation(request);
 		}
     };
-    
-    request.open("GET", "/resources/test.xml", true);
-    request.send();
 
     this.console.log(request.readyState);
 });
@@ -18,13 +18,47 @@ function fillInformation (file) {
 
     var xmlDoc = file.responseXML;
 
-    var movie = null;
-    var movieTitle = "Frozen 2";
-    var movieAuthor = "J.J. Abrams";
+    var movie = xmlDoc.getElementsByTagName("Movie");
 
-    movie = xmlDoc.getElementById("1");
-    //movieTitle = movie.childNodes[0].textContent;
+    console.log(file);
+    console.log(xmlDoc);
+    console.log(movie);
 
-    document.getElementById("OrangePanner").innerHTML = "<h2>" + movieTitle + "</h2>";
+    var movieTitle = "", movieRegie = "", movieYear = "", movieURL = "";
 
-}
+    var ever = document.getElementsByClassName("ever");
+
+    // iterate thru the best 5 Movies ever
+    for (var i = 0; i < 5; i++) {
+        var m = movie[i].childNodes;
+        movieTitle = m[1];
+        movieRegie = m[3];
+        movieYear = m[5];
+        movieURL = m[7];
+
+        var div = document.createElement("div");
+        div.className = "ranked";
+        div.innerHTML = '<h2>' + i + '</h2> <a href="MovieInfo.xhtml"><img class="movie" src="' + movieURL + '" alt="revenant" /></a><div class="description"><h3>' + 
+            movieTitle + '</h3><p>Regie:' + movieRegie +'</p><p>Year:' + movieYear +'</p></div>';
+
+        ever.appendChild(div);
+    }
+
+    var month = document.getElementsByClassName("month");
+
+    // iterate thru the best 5 Movies last month
+    for (var i = 5; i < month.length; i++) {
+        var m = movie[i].childNodes;
+        movieTitle = m[1];
+        movieRegie = m[3];
+        movieYear = m[5];
+        movieURL = m[7];
+
+        var div = document.createElement("div");
+        div.className = "ranked";
+        div.innerHTML = '<h2>' + i + '</h2> <a href="MovieInfo.xhtml"><img class="movie" src="' + movieURL + '" alt="revenant" /></a><div class="description"><h3>' + 
+            movieTitle + '</h3><p>Regie:' + movieRegie +'</p><p>Year:' + movieYear +'</p></div>';
+
+        month.appendChild(div);
+    }
+};
