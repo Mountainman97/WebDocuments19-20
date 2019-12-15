b7d92677df5f016c9e9bb34d58ca35ba2a8fe9a7
Max Bergmann <bergmmax@b-tu.de>
2019-12-15 21:16:21 +0100

    created xml folder and added new xml file for navbar

--- filmbox/src/main/content/javascript/CreateMovies.js
+++ filmbox/src/main/content/javascript/CreateMovies.js
@@ -2,7 +2,7 @@ window.addEventListener("load", function (evt) {
     
     var request = new XMLHttpRequest();
 
-    request.open("GET", "https://mountainman97.github.io/WebDocuments19-20/filmbox/src/main/resources/movies.xml", true);
+    request.open("GET", "https://mountainman97.github.io/WebDocuments19-20/filmbox/src/main/content/xml/movies.xml", true);
     request.send();
 
     request.onreadystatechange = function() {
