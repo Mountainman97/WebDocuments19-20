09d9822d3265ef2480d308567324e4c99c566bb5
Max Bergmann <bergmmax@b-tu.de>
2020-01-19 18:19:16 +0100

    fixed NavBar Bug

--- filmbox/src/main/content/javascript/CreateMovies.js
+++ filmbox/src/main/content/javascript/CreateMovies.js
@@ -2,13 +2,23 @@ window.addEventListener("load", function(evt) {
 
     var request = new XMLHttpRequest();
 
-    request.open("GET", "https://mountainman97.github.io/WebDocuments19-20/filmbox/src/main/content/xml/movies.xml", true);
+    request.open("GET", "https://mountainman97.github.io/WebDocuments19-20/filmbox/src/main/content/xml/nav.xml", true);
     request.send();
 
     request.onreadystatechange = function() {
         if (request.readyState == 4 && request.status == 200) {
             fillInformation(request);
-            fillInformation2(request);
+        }
+    };
+
+    var request2 = new XMLHttpRequest();
+
+    request2.open("GET", "https://mountainman97.github.io/WebDocuments19-20/filmbox/src/main/content/xml/movies.xml", true);
+    request2.send();
+
+    request2.onreadystatechange = function() {
+        if (request2.readyState == 4 && request2.status == 200) {
+            fillInformation2(request2);
         }
     };
 });
